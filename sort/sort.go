2a8c28751efbf1f743dad7c20885d95e5a6588c2
jiangsiyi <157774412+jiangsiyime@users.noreply.github.com>
2024-01-31 14:43:46 +0800

    Update sort.go

--- sort/sort.go
+++ sort/sort.go
@@ -19,3 +19,34 @@ func QuickSort(nums []int, left int, right int) {
     QuickSort(nums, left, flag-1)
     QuickSort(nums, flag+1, right)
 }
+
+// MergeSort merge sort function
+//  - nums: the numbers will be sorted
+func MergeSort(nums []int) (result []int) {
+	if len(nums) <= 1 {
+		return nums
+	}
+	mid := len(nums) / 2
+	left := MergeSort(nums[:mid])
+	right := MergeSort(nums[mid:])
+	return Merge(left, right)
+}
+
+// Merge merge two slices
+//  - left: left slice
+//  - right: right slice
+func Merge(left []int, right []int) (result []int) {
+	i, j := 0, 0
+	for i < len(left) && j < len(right) {
+		if left[i] < right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+	}
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+	return
+}
